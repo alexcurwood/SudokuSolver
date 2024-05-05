92b1db2fede1f394da13f687cf530ff9b86f2f8b
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-05-05 17:27:26 +0100

    debug solveSudoku

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Grid from "./grid";
 
 function App() {
   const [grid, setGrid] = useState([]);
-  const [solution, setSolution] = useState([]);
   const [initialiseGrid, setInitialiseGrid] = useState(false);
+  const [solution, setSolution] = useState([]);
   const [initialiseSolution, setInitialiseSolution] = useState(false);
 
   async function getSudoku() {
@@ -43,19 +43,105 @@ function App() {
         }
       }
       for (let j = 0; j < possibleNumbers.length; j++) {
-        if (isValidValue(possibleNumbers[j])) {
+        if (isValidValue(possibleNumbers[j], currentIndex)) {
+          console.log("hello");
           solution[currentIndex] = possibleNumbers[j];
           break;
         }
-        solution[currentIndex] = 0;
-        i--;
+        if (j == possibleNumbers.length - 1) {
+          console.log("goodbye");
+          solution[currentIndex] = 0;
+          i--;
+        }
       }
-      solution[currentIndex] = 1;
-      // loop through possible numbers
-      // set to the lowest one using checker
-      // if no possible numbers set back to zero and set i--
+
       setSolution(solution);
     }
+  }
+
+  function isValidValue(number, index) {
+    const row1 = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    const row2 = row1.map((index) => index + 9);
+    const row3 = row2.map((index) => index + 9);
+    const row4 = row3.map((index) => index + 9);
+    const row5 = row4.map((index) => index + 9);
+    const row6 = row5.map((index) => index + 9);
+    const row7 = row6.map((index) => index + 9);
+    const row8 = row7.map((index) => index + 9);
+    const row9 = row8.map((index) => index + 9);
+    const rows = [row1, row2, row3, row4, row5, row6, row7, row8, row9];
+
+    const column1 = [0, 9, 18, 27, 36, 45, 54, 63, 72];
+    const column2 = column1.map((index) => index + 1);
+    const column3 = column2.map((index) => index + 1);
+    const column4 = column3.map((index) => index + 1);
+    const column5 = column4.map((index) => index + 1);
+    const column6 = column5.map((index) => index + 1);
+    const column7 = column6.map((index) => index + 1);
+    const column8 = column7.map((index) => index + 1);
+    const column9 = column8.map((index) => index + 1);
+    const columns = [
+      column1,
+      column2,
+      column3,
+      column4,
+      column5,
+      column6,
+      column7,
+      column8,
+      column9,
+    ];
+
+    const topLeftIndeces = [0, 1, 2, 9, 10, 11, 18, 19, 20];
+    const topMiddleIndeces = topLeftIndeces.map((index) => index + 3);
+    const topRightIndeces = topMiddleIndeces.map((index) => index + 3);
+    const middleLeftIndeces = [27, 28, 29, 36, 37, 38, 45, 46, 47];
+    const middleMiddleIndeces = middleLeftIndeces.map((index) => index + 3);
+    const middleRightIndeces = middleMiddleIndeces.map((index) => index + 3);
+    const bottomLeftIndeces = [54, 55, 56, 63, 64, 65, 72, 73, 74];
+    const bottomMiddleIndeces = bottomLeftIndeces.map((index) => index + 3);
+    const bottomRightIndeces = bottomMiddleIndeces.map((index) => index + 3);
+    const subGrids = [
+      topLeftIndeces,
+      topMiddleIndeces,
+      topRightIndeces,
+      middleLeftIndeces,
+      middleMiddleIndeces,
+      middleRightIndeces,
+      bottomLeftIndeces,
+      bottomMiddleIndeces,
+      bottomRightIndeces,
+    ];
+
+    let indecesToCheck = [];
+
+    for (let row of rows) {
+      if (row.includes(index)) {
+        indecesToCheck.push(row);
+      }
+    }
+    for (let column of columns) {
+      if (column.includes(index)) {
+        indecesToCheck.push(column);
+      }
+    }
+    for (let subGrid of subGrids) {
+      if (subGrid.includes(index)) {
+        indecesToCheck.push(subGrid);
+      }
+    }
+
+    console.log(index, number, indecesToCheck);
+
+    for (let array of indecesToCheck) {
+      for (let indexToCheck of array) {
+        if (grid[indexToCheck] == number) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 
   return (
